557f267626fb412f4e9c5d3a0aba978a16cf47d6
Angel Alvarez <angel817jr@gmail.com>
2020-10-19 00:43:57 -0500

    Added enemy AI, and made level layout.

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,15 +1,43 @@
-﻿using UnityEngine;
+﻿using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.AI;
+using Debug = UnityEngine.Debug;
 
 public class EnemyScript : MonoBehaviour
 {
+    [SerializeField]
+    Transform destination = null;
+
+    NavMeshAgent navMeshAgent;
+    private bool navReady;
     public int enemyHealth = 100;
 
+    void Start ()
+    {
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if(navMeshAgent == null)
+        {
+            Debug.LogError("NavMeshAgent not detected on" + gameObject.name);
+        }
+        else
+        {
+            navReady = true;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
         if (enemyHealth <= 0)
         {
             Destroy(gameObject);
+            navReady = false;
+        }
+
+        if(destination != null && navReady)
+        {
+            Vector3 targetVector = destination.transform.position;
+            navMeshAgent.SetDestination(targetVector);
         }
     }
     void DeductPoints(int damageAmount)
---
+++ Assets/Scripts/PlayerReady.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerReady : MonoBehaviour
+{
+    public static bool gunActive;
+    public GameObject gunText;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        gunActive = PickUpGun.gunActive;
+        if(gunActive)
+        {
+            gunText.SetActive(false);
+            Destroy(gameObject);
+        }
+    }
+}
