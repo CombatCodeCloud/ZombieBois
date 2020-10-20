d02857ecfc70f64bafb071debaafee69e85c79ac
Angel Alvarez <angel817jr@gmail.com>
2020-10-20 01:08:50 -0500

    Map almost finished, added easter egg, need to fix navmesh bug.

---
+++ Assets/Scripts/EasterEggs/FirstEgg.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FirstEgg : MonoBehaviour
+{
+    float theDistance = PlayerCasting.distanceFromTarget;
+    public GameObject eggText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        theDistance = PlayerCasting.distanceFromTarget;
+    }
+
+    void OnMouseOver()
+    {
+        if (theDistance <= 6)
+        {
+            eggText.SetActive(true);
+        }
+        else
+        {
+            eggText.SetActive(false);
+        }
+    }
+    void OnMouseExit()
+    {
+        eggText.SetActive(false);
+    }
+}
